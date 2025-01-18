8f178656d38576aa28dc02a57171a58439237d55
Thomas Kroeber <cellcortex@users.noreply.github.com>
2025-01-18 18:42:44 +0100

    Updates

---
+++ _next/static/chunks/pages/_app-8f893f01ac826a1a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{97379:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(74479)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(97379),n(23749))),_N_E=_.O()}]);
