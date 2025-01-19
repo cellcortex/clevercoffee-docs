45d917dd85af87c40fcd067e6f5f8b6d43bafe42
Thomas Kroeber <cellcortex@users.noreply.github.com>
2025-01-19 01:25:42 +0100

    Updates

--- dictionaries/en.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  logo: {
-    title: "React Hooks for Data Fetching"
-  },
-  danger: {
-    title: "Warning",
-    text: "Working with high voltage can be deadly! Always pull the power plug of your coffee maker before touching a screw or opening the housing! Just switching the machine off is not sufficient. Smart or WiFi plugs are not necessarily providing full separation from the mains! The power plug always has to be pulled from the socket! You are working at your own risk and carry all responsibility for any modifications."
-  }
-};
